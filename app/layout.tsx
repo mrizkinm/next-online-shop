659a670d3ab5d265c9c99fb27aa5bb7ed9dadfa5
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-01-13 09:57:56 +0700

    Implement dynamic store information fetching and enhance layout with Navbar and Footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
+import { StoreProvider } from "../context/store-context";
+import { StoreInfo } from "./types";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,24 +17,58 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Gundam Shop",
-  description: "Gundam Online Shop",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  // Ambil data store secara dinamis
+  const storeInfo = await getStoreInfo();
 
-export default function RootLayout({
+  return {
+    title: storeInfo.name,
+    description: storeInfo.address,
+  };
+}
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const storeInfo: StoreInfo = await getStoreInfo();
+
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <NextTopLoader showSpinner={false} />
-        {children}
-      </body>
-    </html>
+    <StoreProvider storeInfo={storeInfo}>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <NextTopLoader showSpinner={false} />
+          <div className="min-h-screen bg-gray-50 pt-[50px] md:pt-[116px]">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </body>
+      </html>
+     </StoreProvider>
   );
+}
+
+async function getStoreInfo(): Promise<StoreInfo> {
+  try {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const res = await fetch(`${baseUrl}/api/data/shop`, { method: "GET" });
+    if (!res.ok) {
+      throw new Error('Failed to fetch store information');
+    }
+    const responseData = await res.json();
+    return responseData[0];
+  } catch (error) {
+    console.error(error);
+    return {
+      name: 'Error',
+      address: 'Error',
+      phone: '/img/default.jpg',
+      email: 'Error',
+      description: '/img/default.jpg'
+    };
+  }
 }
