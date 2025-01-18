90d213bc12e1585fef4983bd66b5e52f74835610
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-01-18 22:24:18 +0700

    Add CartPage component to display shopping cart items and total price

---
+++ app/cart/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import CartItem from '@/components/cart-item';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator'
+import { useCart } from '@/context/cart-context';
+import React from 'react'
+
+const CartPage = () => {
+  const { cart } = useCart();
+  const totalItems = cart.length;
+  const totalPrice = cart.reduce((sum: any, item: any) => sum + (item.product.price * item.quantity), 0);
+
+  return (
+    <div className="max-w-[1240px] px-4 sm:px-6 lg:px-8 mx-auto w-full py-8">
+      <div className="space-y-4">
+        <div>
+          <h1 className="text-3xl font-bold tracking-tight">Cart</h1>
+          <p className="text-sm text-muted-foreground">
+            Daftar semua item di keranjang.
+          </p>
+        </div>
+        <Separator />
+        <div className="space-y-6">
+          <div className="flex justify-between items-center mb-4">
+            <h3 className="font-semibold">Shopping Cart</h3>
+            <Badge variant="secondary">
+              {totalItems} items
+            </Badge>
+          </div>
+          <div className="pr-4">
+            {cart.map((item: any) => (
+              <React.Fragment key={item.id}>
+                <CartItem item={item} />
+                <Separator />
+              </React.Fragment>
+            ))}
+          </div>
+          <div className="mt-4 space-y-4">
+            <div className="flex justify-between font-semibold">
+              <span>Total</span>
+              <span>Rp {totalPrice.toLocaleString()}</span>
+            </div>
+            <div className="space-y-2">
+              <Button className="w-full">
+                Checkout
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CartPage
