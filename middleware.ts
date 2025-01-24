81f4a04a75e39599ea03d40e7ad98b9c3e9a48a2
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-01-24 09:49:19 +0700

    Update middleware to change API matcher from customer to order paths

--- middleware.ts
+++ middleware.ts
@@ -35,5 +35,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/customer/:path*', '/api/customer/:path*'],
+  matcher: ['/customer/:path*', '/api/order/:path*'],
 };
