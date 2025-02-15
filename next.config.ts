907aaedf0c139fabadd505eae9290ff9f986f345
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-15 22:19:19 +0700

    add remote

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'http',
         hostname: 'localhost',
       },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ], // Tambahkan domain di sini
   },
 };
